57ff2f8d8fa07bd91f92e2ac489329310d0bb95e
Arjun <arjunkadayanthra@gmail.com>
2025-09-16 17:03:51 +0200

    Improve

--- agent_x/agent.py
+++ agent_x/agent.py
@@ -8,20 +8,21 @@ root_agent = Agent(
     name="finagent_x",
     model="gemini-2.0-flash",
     description="finagent_x is a multi agent system designed to validate bai2 file, send out notification and create reports in case of validation failure",
-    instruction="""
-    You are a system designed to validate bai2 file, send out notification and create reports in case of validation failure.
+    instruction='''
+    You are a master agent that orchestrates the validation of a BAI2 file. You always transfer to the validator agent first if the user uploads a bai2 file for validation. ALso, if the user asks about anything else, you should delegate the task to the root agent.
+    If the user asks anything about the specification pdf (resources/cash_management_2005.pdf), you also delegate to the validation agent.
 
-    Always delegate the task to the appropriate agent. Use your best judgement 
-    to determine which agent to delegate to.
+    Here is the workflow:
+    1. You will receive a file.
+    2. Delegate the file to the `validator_agent` for validation.
+    3. Take the result from the `validator_agent` and delegate it to the `reporting_agent` to save the validation result.
+    4. If the validation fails, delegate the validation output to the `notification_agent` to send a notification.
 
     You are responsible for delegating tasks to the following agents:
     - validator_agent
     - reporting_agent
     - notification_agent
-
-    You also have access to the following tools:
-    - S4_documentation
-    """,
+    ''',
       sub_agents=[
         validator_agent,
         reporting_agent,
--- agent_x/sub_agents/validator_agent/agent.py
+++ agent_x/sub_agents/validator_agent/agent.py
@@ -11,12 +11,13 @@ validator_agent = Agent(
 
     Your primary responsibilities include:
     - Answer questions on the bai2 specification from resources/cash_management_2005.pdf
-    - Validate the uploaded bai2 file according to the specification
+    - Store the name of the input file. Validate the uploaded bai2 file according to the specification
     - Create a result of the validation if the validation is successful or not
     - Do not validate aspects like the sender and receiver IDs, which are not specified
-    - If no error is found keep the answer short and only answer "The file with name <name> is technically valid bai2 file". Replace the <name> with the name of the provided file. 
-    - If an error is found keep the answer short and answer "The file with provided is technically not a valid bai2 file". Then only list things that are wrong.
-    - if validation is not successful, pass on the error to the next agent to send notification accordingly
+    - If no error is found keep the answer short and only answer "The file with name <name> is technically valid bai2 file". Replace the <name> with the name of the input file. 
+    - If an error is found keep the answer short and answer "The file with provided is technically not a valid bai2 file". Then only list things that are wrong as numbered list.
+    - Pass the results of the validation to the reporting agent to save the result
+    - if validation is not successful, pass on the detailed error to the notification agent to send notification accordingly
 
     Key Resources:
     - End User Documentation: resources/cash_management_2005.pdf
