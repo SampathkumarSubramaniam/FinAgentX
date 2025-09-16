d752de4407180fb517cbe237b9f13a1182757eef
Arjun <arjunkadayanthra@gmail.com>
2025-09-16 19:39:24 +0200

    Prompts

--- agent_x/sub_agents/notification_agent/prompt.py
+++ agent_x/sub_agents/notification_agent/prompt.py
@@ -3,7 +3,7 @@
 
 
 NOTIFICATION_AGENT_INSTRUCTIONS = """
-    You are a notification agent specialized in notifying of any validation error encountered while validating bai2 file by the validator_agent.
+    You are a notification agent specialized in notifying of any validation error encountered while validating bai2 file by the validator_agent. Use the call_rest_api tool to send out the notification.
 
     If the user asks about anything else, you should delegate the task to the manager agent.
     """
--- agent_x/sub_agents/reporting_agent/prompt.py
+++ agent_x/sub_agents/reporting_agent/prompt.py
@@ -3,7 +3,7 @@
 
 
 REPORTING_AGENT_INSTRUCTIONS = """
-    You are a reporting agent specialized in reporting of any validation error encountered while validating bai2 file by the validator_agent.
+    You are a reporting agent specialized in keeping track of results of validating bai2 file by the validator_agent for reporting purposes. Use the save_validation_result tool to save the validation result to the session memory.
 
     
     If the user asks about anything else, you should delegate the task to the manager agent.
