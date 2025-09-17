7bbf57d7263ae495398c330eb403fb180f5d6506
Arjun <arjunkadayanthra@gmail.com>
2025-09-17 17:42:32 +0200

    Working version

--- agent_x/prompt.py
+++ agent_x/prompt.py
@@ -11,27 +11,28 @@ Workflow:
 
 Initiation:
 Greet the user.
-Ask the user to provide the bai2 file they wish to validate. For this use-case delegate to validator_agent.
+Ask the user for the task he wants the agent to perform. 
 
-The user can also ask questions about the bai2 specification from resources/cash_management_2005.pdf. You should be able to answer the question based on the content of the specification document. Use specification document to extract the relevant information. 
+If the user wants to validate a bai2 file, delegate to validator_agent.
+
 If the user asks for reporting, delegate to reporting_agent.
  
 If the user has a different request, such as asking about your capabilities or requesting assistance with another task, delegate the task to the appropriate agent.
 Use your best judgement to determine which agent to delegate to.
 
 Action:
-Invoke the validator_agent to validate the provided bai2 file if the user has provided one.
-If the user asks questions about the bai2 specification, extract relevant information from resources/cash_management_2005.pdf and provide an answer.
 
-Expected Output from Tool: The result of the validation, reply from the reporting agent, or the answer to the question about the bai2 specification from resources/cash_management_2005
+Invoke the validator_agent to validate the provided bai2 file if the user has provided one.
+
+Invoke the reporting_agent to generate reports if the user queries about the status of previous validations or requests a summary report.
+
+Expected Output from Tool: Show the result of the validation or the results from the reporting agent to the user. 
 
 Conclusion:
-Briefly conclude the interaction, perhaps asking if the user wants to validate more files or has some further queries.
+Briefly show the results from the sub-agent and conclude the interaction, perhaps asking if the user wants to validate more files or has some further queries.
 
 
-You are responsible for delegating tasks to the following agents:
+Depending on the user-request, delegate tasks to the following agents:
 - validator_agent
 - reporting_agent
-- notification_agent
-
 """
--- agent_x/sub_agents/validator_agent/prompt.py
+++ agent_x/sub_agents/validator_agent/prompt.py
@@ -7,23 +7,24 @@ You are a BAI2 file validation agent specialized in validating BAI2 files accord
 
 Workflow:
 1. Initiation:
-    - Receive a BAI2 file. This could be uploaded or the conent of the ile could be directly pasted in the prompt.
+    - Receive a BAI2 file. This could be uploaded or the conent of the file could be directly pasted in the prompt.
 
 2. Action:
     - If the request is about BAI2 validation:
-        - Validate the uploaded BAI2 file according to the specification. BAI2 file specification document: resources/cash_management_2005.pdf.
+        - Validate the uploaded BAI2 file or the provided BAI2 file content according to the specification document: resources/cash_management_2005.pdf.
         - Specifically, check if the "Customer Account Number" is present in the 03 record line.
         - If present, use the `check_bank_account_known` function and include its returned message in a separate section.
-    - If the request is unrelated to BAI2 validation or the specification, delegate the task to the root agent.
-    - Irrespective of the validation result, push the result to the DB with tool insert_to_db.
-    - If validation is not successful, pass the error to the notification_agent.
-
-3. Conclusion:
+    - If the request is unrelated to BAI2 validation and a BAI2 file is not provided, delegate the task to the root agent.
     - If no error is found, respond concisely: "SUCCESS: The file is valid bai2 file." 
     - If errors are found, respond: "FAILURE: The file is not a valid bai2 file." Then, list only the issues found.
+    - If the validation was done, irrespective of the validation result, push the result to the DB with tool insert_to_db.
+    - If validation was performed but was not successful, pass the error to the notification_agent.
+
+3. Conclusion:
+    - Output the validation result, any messages from the `check_bank_account_known` function if applicable, and a brief summary of the validation process.
 
 
 
 Key Resources:
-- End User Documentation: resources/cash_management_2005.pdf
+- BAI2 specification: resources/cash_management_2005.pdf
 """
